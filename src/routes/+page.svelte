18314b28bf6f58ac9afdc7ad2639efd6d81d0e86
Tyler Epperson <tepperson22@gmail.com>
2025-01-06 00:01:31 -0500

    Add a jekyll ignore file. Update a few minor things.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,2 @@
-<h1>My corner of the internet,<br>Tyler Epperson - Software Engineer</h1>
+<h1>Hi, I'm Tyler</h1>
+<p>I'm a software engineer. I build software to solve real problems in the simpliest way. I obesses over the details in the code and making the best possible product.</p>
